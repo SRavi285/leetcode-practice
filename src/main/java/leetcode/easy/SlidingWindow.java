47026262b92b8213a0a415f08e6abf53e78f9404
Ravi Sharma <sravikant828@gmail.com>
2024-08-11 22:28:18 +0530

    findMaxAverage

--- src/main/java/leetcode/easy/SlidingWindow.java
+++ src/main/java/leetcode/easy/SlidingWindow.java
@@ -20,4 +20,31 @@ public class SlidingWindow {
         return result;
     }
 
+    /**
+     * Find maximum sum of a sub array of size K
+     *
+     * @param numbs
+     * @param index
+     * @return int
+     * @TimeComplexity O(n)
+     * @SpaceComplexity O(1)
+     * @TimeTaken 35ms
+    */
+    public double findMaxAverage (int[] numbs, int index) {
+        int n = numbs.length;
+        double maxAverage =  Double.NEGATIVE_INFINITY;
+        double windowSum = 0;
+        for (int i = 0; i < index; i++) {
+            windowSum += numbs[i];
+        }
+        maxAverage = windowSum / index;
+
+        for (int right = index; right < n; right++) {
+            windowSum += numbs[right] - numbs[right - index];
+            maxAverage = Math.max(maxAverage, (double) windowSum / index);
+        }
+        System.out.println(maxAverage);
+        return maxAverage;
+    }
+
 }
--- src/test/java/leetcode/easy/SlidingWindowTest.java
+++ src/test/java/leetcode/easy/SlidingWindowTest.java
@@ -7,11 +7,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SlidingWindowTest {
 
+
     SlidingWindow  slidingWindow = new SlidingWindow();
 
     @Test
     void longestSubString() {
         assertEquals(5, slidingWindow.longestSubString("abcdbea"));
         assertEquals(3, slidingWindow.longestSubString("pwwkew"));
+    }
+
+
+
+
+    @Test
+    void findMaxAverage() {
+//        assertEquals(12.75, slidingWindow.findMaxAverage(new int[]{1, 12, -5, -6, 50, 3}, 4));
+        double result1 = slidingWindow.findMaxAverage(new int[]{1, 12, -5, -6, 50, 3}, 4);
+        assertEquals(12.75, result1, 1e-5);
+        double result2 = slidingWindow.findMaxAverage(new int[]{5}, 1);
+        assertEquals(5.0, result2, 1e-5);
     }
 }
